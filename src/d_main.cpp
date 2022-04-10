f8896e07c99455f59fa79fb732e434d19f25dd96
Christoph Oelckers <coelckers@users.noreply.github.com>
2022-04-10 08:54:45 +0200

    - first stage of migrating intermissions to screenjobs.

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -118,6 +118,7 @@
 #include "hw_clock.h"
 #include "hwrenderer/scene/hw_drawinfo.h"
 #include "doomfont.h"
+#include "screenjob.h"
 
 #ifdef __unix__
 #include "i_system.h"  // for SHARE_DIR
@@ -1016,18 +1017,20 @@ void D_Display ()
 				End2DAndUpdate ();
 				return;
 				
-			case GS_INTERMISSION:
-				WI_Drawer ();
-				break;
-				
-			case GS_FINALE:
-				F_Drawer ();
-				break;
-				
 			case GS_DEMOSCREEN:
 				D_PageDrawer ();
 				break;
 				
+		case GS_CUTSCENE:
+		case GS_INTRO:
+			if (ScreenJobTick())
+			{
+				// synchronize termination with the playsim.
+				Net_WriteByte(DEM_ENDSCREENJOB);
+			}
+			break;
+
+
 			default:
 				break;
 		}
@@ -1166,7 +1169,6 @@ void D_ErrorCleanup ()
 	{
 		menuactive = MENU_Off;
 	}
-	if (gamestate == GS_INTERMISSION) gamestate = GS_DEMOSCREEN;
 	insave = false;
 	ClearGlobalVMStack();
 }
@@ -2733,7 +2735,7 @@ static bool System_CaptureModeInGame()
 	case 0:
 		return gamestate == GS_LEVEL;
 	case 1:
-		return gamestate == GS_LEVEL || gamestate == GS_INTERMISSION || gamestate == GS_FINALE;
+		return gamestate == GS_LEVEL || gamestate == GS_CUTSCENE;
 	case 2:
 		return true;
 	}
@@ -2973,7 +2975,6 @@ extern DThinker* NextToThink;
 
 static void GC_MarkGameRoots()
 {
-	GC::Mark(DIntermissionController::CurrentIntermission);
 	GC::Mark(staticEventManager.FirstEventHandler);
 	GC::Mark(staticEventManager.LastEventHandler);
 	for (auto Level : AllLevels())
@@ -3692,7 +3693,6 @@ void D_Cleanup()
 	G_ClearMapinfo();
 
 	M_ClearMenus();					// close menu if open
-	F_EndFinale();					// If an intermission is active, end it now
 	AM_ClearColorsets();
 	DeinitSWColorMaps();
 	FreeSBarInfoScript();
